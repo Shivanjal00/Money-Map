32b079b4ae6aa26451ba0d7f224d40c33d7339ec
Shivanjal_walkingtree <shivanjal@walking.tech>
2024-08-12 17:37:23 +0530

    first commit

---
+++ app/src/main/java/com/example/moneymap/HomeScreen.kt
@@ -0,0 +1,22 @@
+package com.example.moneymap
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Constraints
+
+@Composable
+fun HomeScreen(){
+    Surface (modifier = Modifier.fillMaxSize()){
+
+    }
+
+}
+
+@Composable
+@Preview(showBackground = true)
+fun PreviewHomeScreen(){
+    HomeScreen()
+}
